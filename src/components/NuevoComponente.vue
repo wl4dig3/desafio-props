67e087e33a729b7d2191438295330aef166443a1
Wladimir Guevara <wladige@gmail.com>
2020-09-11 00:33:23 -0300

    fin

--- src/components/NuevoComponente.vue
+++ src/components/NuevoComponente.vue
@@ -3,11 +3,10 @@
     <h1 class="my-3">{{message1}}</h1>
     <form action>
         <b-form-input v-model="item" placeholder="Ingresa aqui una tarea nueva"></b-form-input>
-
     </form>
-    <b-button variant="success" class="m-1" @click="add"> Add</b-button>
+    <b-button variant="success" class="m-1" @click="add">Add</b-button>
 
-    <b-button variant="danger" class="m-1" @click="sustract"> Sustract</b-button>
+    <b-button variant="danger" class="m-1" @click="sustract">Sustract</b-button>
     <ul v-for="item of items">
         <li>{{ item }}</li>
     </ul>
@@ -24,23 +23,20 @@ export default {
         return {
             message1: "Desafio de uso con props en Vue",
 
-            texto: "aqui saldrá la la tarea",
             items: [],
-            item: '',
+            item: "",
         };
     },
     methods: {
-
         add() {
             this.items.push(this.item);
-            this.item = '';
+            this.item = "";
         },
         sustract() {
             this.items.pop(this.item);
-            this.item = '';
-
+            this.item = "";
         },
-    }
+    },
 };
 </script>
 
