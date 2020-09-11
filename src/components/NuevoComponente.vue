ec848471ef3037ba3c11018a66b930385bd4a468
Wladimir Guevara <wladige@gmail.com>
2020-09-10 21:32:43 -0300

    agregando nuevo componente

---
+++ src/components/NuevoComponente.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <h1>{{message1}}</h1>
+    <form action="">
+        <input type="text" placeholder="Add To list">
+        <button>+</button>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'NuevoComponente',
+    props: {
+        msssg: String
+    },
+    data() {
+        return {
+            message1: 'soy el nuevo componente en NUevo Componente '
+        }
+    }
+}
+</script>
