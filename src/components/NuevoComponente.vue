1d8424388be5872fce80dbfaa88585a19838ff2b
Wladimir Guevara <wladige@gmail.com>
2020-09-10 22:13:07 -0300

    agregando bootstrap

--- src/components/NuevoComponente.vue
+++ src/components/NuevoComponente.vue
@@ -1,23 +1,26 @@
 <template>
-<div>
-    <h1>{{message1}}</h1>
-    <form action="">
-        <input type="text" placeholder="Add To list">
-        <button>+</button>
+<div class="container">
+    <h1 class="my-3">{{message1}}</h1>
+    <form action>
+        <b-form-input v-model="text" placeholder="Ingresa aqui una tarea nueva"></b-form-input>
+        <div class="mt-2">Value: {{ texto }}</div>
     </form>
+    <b-button variant="success" class="m-1">Add</b-button>
 </div>
 </template>
 
 <script>
 export default {
-    name: 'NuevoComponente',
+    name: "NuevoComponente",
     props: {
-        msssg: String
+        msssg: String,
     },
     data() {
         return {
-            message1: 'soy el nuevo componente en NUevo Componente '
-        }
-    }
-}
+            message1: "Desafio de uso con props en Vue",
+
+            texto: "aqui saldrá la la tarea",
+        };
+    },
+};
 </script>
