c59ff3b7646eb95123d22eab3e81d745601e4baf
Wladimir Guevara <wladige@gmail.com>
2020-09-11 00:20:37 -0300

    terminacion de funcionabilidad a los botones add y sustract, estilos a ul

--- src/components/NuevoComponente.vue
+++ src/components/NuevoComponente.vue
@@ -2,10 +2,15 @@
 <div class="container">
     <h1 class="my-3">{{message1}}</h1>
     <form action>
-        <b-form-input v-model="text" placeholder="Ingresa aqui una tarea nueva"></b-form-input>
-        <div class="mt-2">Value: {{ texto }}</div>
+        <b-form-input v-model="item" placeholder="Ingresa aqui una tarea nueva"></b-form-input>
+
     </form>
-    <b-button variant="success" class="m-1">Add</b-button>
+    <b-button variant="success" class="m-1" @click="add"> Add</b-button>
+
+    <b-button variant="danger" class="m-1" @click="sustract"> Sustract</b-button>
+    <ul v-for="item of items">
+        <li>{{ item }}</li>
+    </ul>
 </div>
 </template>
 
@@ -20,7 +25,28 @@ export default {
             message1: "Desafio de uso con props en Vue",
 
             texto: "aqui saldrá la la tarea",
+            items: [],
+            item: '',
         };
     },
+    methods: {
+
+        add() {
+            this.items.push(this.item);
+            this.item = '';
+        },
+        sustract() {
+            this.items.pop(this.item);
+            this.item = '';
+
+        },
+    }
 };
 </script>
+
+<style lang="scss" scoped>
+ul {
+    list-style: none;
+    font-size: 1.2em;
+}
+</style>
